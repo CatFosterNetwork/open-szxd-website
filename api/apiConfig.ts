d37d7ce47985766fe926ef7299ec3fc9c78a27f7
Fei-xiangShi <595834560@qq.com>
2024-03-19 10:47:22 +0800

    api url updated

--- api/apiConfig.ts
+++ api/apiConfig.ts
@@ -1,5 +1,5 @@
 
-// const baseUrl = "http://open.szxd.swu.social/api";
+// const baseUrl = "https://open.szxd.swu.social/api";
 const baseUrl = "http://redmiax6s.lan:8080/api";
 // api 文档 https://app.apifox.com/project/4156592
 
