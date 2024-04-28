58022cd0864167ef1ae116ad3c82d462871d2e96
Fei-xiangShi <595834560@qq.com>
2024-04-28 23:53:49 +0800

    update

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -34,7 +34,7 @@ const close = () => {
           <h2 class="text-lg font-semibold">温馨提示</h2>
           <p class="mt-2 text-sm text-gray-600">
             开放数智西大在节假日期间将会自动检测您的请假状态，如果您处于请假状态，将不会进行自动打卡操作。手动销假过后会恢复自动打卡（若打卡仍未过期）。
-            在节假日期间，若您不在学校，您可以通过网页VPN或者客户端VPN进入 <a herf="http://open-szxd-swu-social-s.sangfor.vpn.swu.edu.cn:8118/">http://open-szxd-swu-social-s.sangfor.vpn.swu.edu.cn:8118/</a>
+            在节假日期间，若您不在学校，您可以通过网页VPN或者客户端VPN进入 <NuxtLink to="http://open-szxd-swu-social-s.sangfor.vpn.swu.edu.cn:8118/">http://open-szxd-swu-social-s.sangfor.vpn.swu.edu.cn:8118/</NuxtLink>
             来访问我们的服务!
           </p>
           <div class="mt-4 flex justify-end">
