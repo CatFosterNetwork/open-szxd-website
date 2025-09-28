309d3228bd6de31c80c4b2c8afceeb5c34124ac5
Fei-xiangShi <595834560@qq.com>
2025-09-28 23:19:46 +0800

    update

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -4,6 +4,8 @@ import type { Period, Range } from "~/types";
 
 checkLogin();
 
+const { t } = useI18n();
+
 const range = ref<Range>({
   start: sub(new Date(), { days: 14 }),
   end: new Date(),
@@ -19,7 +21,7 @@ if (hasOpened.value as any != true) {
 }
 
 const open = ref(true)
-const isOpened = computed(() => !open.value && hasOpened.value as any == true ? false : true);
+const isOpened = computed(() => hasOpened.value as any == true ? false : true);
 
 const close = () => {
   hasOpened.value = true as any;
@@ -31,25 +33,23 @@ const close = () => {
     <UDashboardPage>
       <UModal v-model="isOpened">
         <div class="p-4">
-          <h2 class="text-lg font-semibold">温馨提示</h2>
+          <h2 class="text-lg font-semibold">{{ t('dashboard.modals.warmReminder.title') }}</h2>
           <p class="mt-2 text-sm text-gray-500">
-            开放数智西大在节假日期间将会自动检测您的请假状态，如果您处于请假状态，将不会进行自动打卡操作。手动销假过后会恢复自动打卡（若打卡仍未过期）。
-            在节假日期间，若您不在学校，您可以通过网页VPN或者客户端VPN进入 <NuxtLink class="link hover:text-green-500" to="http://10-132-98-229-8000-p.sangfor.vpn.swu.edu.cn:8118/">http://10-132-98-229-8000-p.sangfor.vpn.swu.edu.cn:8118/</NuxtLink>
-            来访问我们的服务!
+            {{ t('dashboard.modals.warmReminder.content') }}
           </p>
           <div class="mt-4 flex justify-end">
-            <UButton @click="close">Got it!</UButton>
+            <UButton @click="close">{{ t('dashboard.modals.warmReminder.button') }}</UButton>
           </div>
         </div>
       </UModal>
       <UModal v-model="open">
         <div class="p-4">
-          <h2 class="text-xl font-semibold">域名更换提示</h2>
+          <h2 class="text-xl font-semibold">{{ t('dashboard.modals.domainChange.title') }}</h2>
           <p class="mt-2 text-base text-gray-500">
-            由于运营商屏蔽，我们将域名从原域名迁移至 <NuxtLink class="link hover:text-green-500" to="https://38.147.184.134/">https://38.147.184.134/</NuxtLink> 原有服务不受影响，原域名将会在一段时间后停止服务，请您尽快更换域名来访问我们的服务!
+            {{ t('dashboard.modals.domainChange.content') }} <NuxtLink class="link hover:text-green-500" to="https://38.147.184.134/">https://38.147.184.134/</NuxtLink> {{ t('dashboard.modals.domainChange.contentSuffix') }}
           </p>
           <div class="mt-4 flex justify-end">
-            <UButton @click="open=false">Got it!</UButton>
+            <UButton @click="open=false">{{ t('dashboard.modals.domainChange.button') }}</UButton>
           </div>
         </div>
       </UModal>
